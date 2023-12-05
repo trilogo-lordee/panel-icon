6a3ae489d28eee9e08a96c562f1dd4856d2f3597
mr.lordee <support@trilogo.dp.ua>
2023-12-05 17:44:10 +0200

    generator install

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import generator from "../generator/index.js";
+import generator from "generator";
 
 export default defineConfig({
     plugins: [
